35bd6a48761855c80230f403c51f71558b78f236
rubyonrials <matthew.rials+github2@gmail.com>
2023-01-10 20:53:26 -0800

    init commit, working go server that serves json tokens for 3 channels

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+    "encoding/json"
+    "github.com/livekit/protocol/auth"
+    "github.com/google/uuid"
+)
+
+const ApiKey = "devkey"
+const ApiSecret = "secret"
+
+func issueTokens(w http.ResponseWriter, r *http.Request) {
+    tokens := make(map[string]string)
+    redToken, err1 := getJoinToken("red");
+    blueToken, err2 := getJoinToken("blue");
+    greenToken, err3 := getJoinToken("green");
+
+    if err1 == nil && err2 == nil && err3 == nil {
+      tokens["red"] = redToken;
+      tokens["blue"] = blueToken;
+      tokens["green"] = greenToken;
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(tokens)
+}
+
+func getJoinToken(room string) (string, error) {
+    canPublish := true
+    canSubscribe := true
+
+    at := auth.NewAccessToken(ApiKey, ApiSecret)
+    grant := &auth.VideoGrant{
+        RoomJoin:     true,
+        Room:         room,
+        CanPublish:   &canPublish,
+        CanSubscribe: &canSubscribe,
+    }
+    at.AddGrant(grant).
+        SetIdentity(uuid.New().String()).
+        SetValidFor(time.Hour)
+
+    return at.ToJWT()
+}
+
+func main() {
+    http.HandleFunc("/issue-tokens", issueTokens)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
